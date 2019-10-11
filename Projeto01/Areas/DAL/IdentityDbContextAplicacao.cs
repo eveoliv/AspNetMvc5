0e813a795dee668e4f35cc6c84577f1c2bba6b20
Everton Oliveira <everton_ace@hotmail.com>
2019-10-10 22:57:34 -0300

    Implementacoes de seguranca 7.2

---
+++ Projeto01/Areas/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Projeto01.Areas.Seguranca.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Projeto01.Areas.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("identitydb"){}
+
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(new IdentityDbInit()); 
+        }
+
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();               
+        }
+
+        private class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao>{}
+    }
+}
