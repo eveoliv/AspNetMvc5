09191b33c751696f0666ab3851e06cfdd4626f25
Everton Oliveira <everton_ace@hotmail.com>
2019-10-14 20:50:57 -0300

    Resgringindo acessos 7.9

--- Projeto01/Areas/Seguranca/Models/SeguracaViewModelos.cs
+++ Projeto01/Areas/Seguranca/Models/SeguracaViewModelos.cs
@@ -15,6 +15,13 @@ namespace Projeto01.Areas.Seguranca.Models
         public string Email { get; set; }
         [Required]
         public string Senha { get; set; }
+    }
 
+    public class LoginViewModel
+    {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Senha { get; set; }
     }
 }
