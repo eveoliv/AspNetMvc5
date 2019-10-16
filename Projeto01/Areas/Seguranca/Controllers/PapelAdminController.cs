0d765397acc3d7089a3042b5e7c51cfbf016199c
Everton Oliveira <everton_ace@hotmail.com>
2019-10-15 23:38:01 -0300

    Implementanco cadastro de papeis 7.10

--- Projeto01/Areas/Seguranca/Controllers/PapelAdminController.cs
+++ Projeto01/Areas/Seguranca/Controllers/PapelAdminController.cs
@@ -24,5 +24,46 @@ namespace Projeto01.Areas.Seguranca.Controllers
                     GetUserManager<GerenciadorPapel>();
             }
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        private GerenciadorUsuario UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().
+                GetUserManager<GerenciadorUsuario>();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create([Required]string nome)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = RoleManager.Create(new Papel(nome));
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(nome);
+        }
     }
 }
