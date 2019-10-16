32af5f0f1ae0260add553785dcc02ff9483f1f12
eoliveira <eoliveira@orizon.com.br>
2019-10-16 16:12:23 -0300

    Revisando Edit de papeis 7.10

--- Projeto01/Areas/Seguranca/Controllers/PapelAdminController.cs
+++ Projeto01/Areas/Seguranca/Controllers/PapelAdminController.cs
@@ -2,7 +2,10 @@
 using Microsoft.AspNet.Identity.Owin;
 using Projeto01.Areas.Seguranca.Models;
 using Projeto01.Infraestrutura;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +13,7 @@ namespace Projeto01.Areas.Seguranca.Controllers
 {
     public class PapelAdminController : Controller
     {
+
         // GET: Seguranca/PapelAdmin
         public ActionResult Index()
         {
@@ -64,6 +68,63 @@ namespace Projeto01.Areas.Seguranca.Controllers
                 }
             }
             return View(nome);
+        }
+
+        public class PapelEditModel
+        {
+            public Papel Papel { get; set; }
+            public IEnumerable<Usuario> Membros { get; set; }
+            public IEnumerable<Usuario> NaoMembros { get; set; }
+        }
+
+        public class PapelModificationModel
+        {
+            [Required]
+            public string NomePapel { get; set; }
+            public string[] IdsParaAdicionar { get; set; }
+            public string[] IdsParaRemover { get; set; }
+        }
+
+        public ActionResult Edit(string id)
+        {
+            Papel papel = RoleManager.FindById(id);
+            string[] memberIDs = papel.Users.Select(x => x.UserId).ToArray();
+            IEnumerable<Usuario> membros = UserManager.Users.Where(x => memberIDs.Any(y => y == x.Id));
+            IEnumerable<Usuario> naoMembros = UserManager.Users.Except(membros);
+            return View(new PapelEditModel
+            {
+                Papel = papel,
+                Membros = membros,
+                NaoMembros = naoMembros
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PapelModificationModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.IdsParaAdicionar ?? new string[] { })
+                {
+                    result = UserManager.AddToRole(userId, model.NomePapel);
+                    if (!result.Succeeded)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+                }
+                foreach (string userId in model.IdsParaRemover ?? new string[] { })
+                {
+                    result = UserManager.RemoveFromRole(userId, model.NomePapel);
+                    if (!result.Succeeded)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.
+                        BadRequest);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         }
     }
 }
